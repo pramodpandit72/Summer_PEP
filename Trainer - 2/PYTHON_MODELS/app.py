edae1889b4eb80f23c25956376102a0c40c4ea35
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-07-30 10:55:57 +0530

    Add Streamlit app for facial emotion detection using Hugging Face model; include usage notes

---
+++ Trainer - 2/PYTHON_MODELS/app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from PIL import Image
+from transformers import pipeline
+import torch
+
+st.set_page_config(page_title="Emotion Detection")
+st.title("Facial Emotion Detection Model Using Hugging Face")
+
+def load_model():
+    return pipeline("image-classification", 
+                    model="trpakov/vit-face-expression"
+                    )
+
+classifier = load_model()
+
+upload_file=st.file_uploader(
+    "Upload a face image",
+    type=["jpg","jpeg","png"]
+)
+
+if upload_file:
+    image=Image.open(upload_file).convert("RGB")
+    st.image(image, caption="Uploaded image", use_container_width=True)
+
+    with st.spinner("Dectecting Emotion"):
+        prediction = classifier(image)
+
+    st.subheader("Predictions")
+
+    for pred in prediction[:5]:
+        st.write(
+            f"**{pred['label']}**: {pred['score']*100 :.2f}%"
+        )
+
+    best = prediction[0]
+
+    st.success(
+        f"Detected Emotion:{best['label']} ({best['score']*100 :.2f}%)"
+    )
---
+++ Trainer - 2/PYTHON_MODELS/notes.md
@@ -0,0 +1,27 @@
+# Transformaers
+- Vision Transformer
+Deep learning model based on the Transformer architecture for image classification. 
+
+Approach...?
+
+1. Take an Image
+2. Split that into patches
+3. Convert it into embeddings
+4. Use transformer and encode
+5. Predict
+
+# Hugging Face Transformer models. 
+- Is an Open Source AI plaform that builds Pt ML models, libraries and tools.
+  
+  1. Github
+  2. Hugging face Hub
+  3. Docker Hub-> Docker Images
+   
+# Contributors:
+- Meta
+- Google
+- OpenAI
+  
+
+
+<!-- // streamlit run app.py -->
